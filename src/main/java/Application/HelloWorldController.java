14a84fa7321054514b4dd546d7ce72ab5634c4f5
pratik <pratikgalave16@gmail.com>
2023-10-23 21:30:03 +0530

    working jdbc connection test
    Signed-off-by: pratik <pratikgalave16@gmail.com>

--- src/main/java/Application/HelloWorldController.java
+++ src/main/java/Application/HelloWorldController.java
@@ -1,4 +1,4 @@
-package Controller;
+package Application;
 
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -14,37 +14,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import repository.UserRepo;
+
 
 @RestController
 @ResponseBody
 public class HelloWorldController {
 	
-	@Autowired
-	UserRepo userRepo ;
 	
 	@RequestMapping("/test")
 	public String hello() {
 	return "HELLO Guys ... I am back.!! :)";
 }
+	
 	@RequestMapping("/names")
 	public  ArrayList<String> getAllUserNames() throws Exception{
-		String query ="select * from students";
-		String url ="jdbc:postgresql://localhost:5432/students";
+		java.sql.Connection	con = null;
+		String query ="select * from studs";
+		String url ="jdbc:postgresql://localhost:5432/pratik_jdbc";
 		String username = "postgres";
-		String passward ="Prati@514";
+		String passward ="Pratik@514";
 		ArrayList<String> nameList = new ArrayList<String>();
-		java.sql.Connection	con	=  DriverManager.getConnection((url),(username),(passward));
+		
+		try {
+			con = DriverManager.getConnection((url),(username),(passward));
+			System.out.println("connected to DB");
+		} catch (SQLException e) {
+			System.out.println("error while connecting to DB");
+			e.printStackTrace();
+		}
+
 		Statement stmt = con.createStatement();
 		ResultSet rs = stmt.executeQuery(query);
 		while(rs.next()) {
-			nameList.add(rs.getString("Name"));
+			nameList.add(rs.getString("name"));
 			nameList.add(rs.getString("location"));
 			
 		}
 		con.close();
-		return nameList;
-			
-		
+		return nameList;	
 	}
 	}
